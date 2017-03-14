23ea4c3d144de304549074cafffabaed6621954e
dsx2017 <dsx2017@sina.com>
2017-03-14 13:11:11 +0800

    wx
    wx

---
+++ src/mock/base-person.js
@@ -0,0 +1,46 @@
+//个人,群 公众号 基本　数据模型
+/*
+  id
+  name
+  wxid
+  qq
+  email
+  type  [friends:好友/group:群/service:服务号/sub:订阅号]
+  iconSrc
+  qrCode
+  signature   //签名
+  newsUnreadCount {Number} //未读
+  endTimeStr    //最后消息
+  endChatAuth   //最后消息作者
+  endChatTxt    //最后消息内容
+  chatBackground //聊天背景
+  newsMute  {Boolean} //消息免打扰
+  area
+  selfPhotos
+  telphone
+ */
+module.exports = {
+    "base": {
+        "id": 1,
+        "nickname": "大双",
+        "remark":"大双",
+        "wxid": "13545631471",
+        "qq": "00001",
+        "email": "leaf4669805@gmail.com",
+        "type": "firends",
+        "iconSrc": "//images2015.cnblogs.com/blog/528573/201609/528573-20160922181803449-1753077357.jpg",
+        "qrCode": "",
+        "telphone":13545631471,
+        "signature": "个性签名",
+        "area":["中国","广东","深圳"],
+        "selfPhotos":[{imgSrc:""}]
+    },
+    "chatPrevivw": {
+        "newsUnreadCount": 1, //未读
+        "endTimeStr": 1472632586443,
+        "endChatAuth": "大双",
+        "endChatTxt": "晚上打球",
+        "chatBackground": null, //聊天背景
+        "newsMute": false
+    }
+}
